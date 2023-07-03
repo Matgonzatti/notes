e0db1a2e1abaa96b219b54203c3c1ff8f2ea7eb3
Matheus <matheus.gonzatti@gmail.com>
2023-07-03 20:19:57 -0300

    refactoring and first component

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+import { Button } from '../../components/Button';
+
+export function Details() {
+  
+  return(
+    <Container>
+      <h1>Hello World!</h1>
+      <span>Matheus Gonzatti</span>
+      <Button title="Login" loading />
+      <Button title="Cadastrar" />
+    </Container>
+  )
+}
